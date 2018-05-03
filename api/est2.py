79c79ec65bfcf57612d42515fceb481ca0e28ab5
chen yao <yao_great@aliyun.com>
2018-05-03 12:18:25 +0800

    之前的模型居然写错了，现在可以回归到正确的值了

--- api/est2.py
+++ api/est2.py
@@ -4,20 +4,19 @@ import numpy as np
 import random
 
 def my_model_fn(features, labels, mode, params, config):
-	X1 = features['X1']
-	X2 = features['X2']
+	X = features['X']
 	Y = labels
-	print("X1:" + str(X1))
-	print("X2:" + str(X2))
+	print("X:" + str(X))
 	print("Y:" + str(Y))
-	W1 = tf.Variable(tf.truncated_normal((1,1)), name="W1")
-	W2 = tf.Variable(tf.truncated_normal((1,1)), name="W2")
-	b = tf.Variable(tf.truncated_normal((1,)), name="b")
-	_Y = tf.matmul(X1, W1) + tf.matmul(X2, W2) + b
+	print("params:" + str(params))
+	W = tf.Variable(tf.truncated_normal((2,1)), name="W")
+	b = tf.Variable(0., name="b")
+	_Y = tf.reshape(tf.matmul(X, W) + b, (-1,))
 	if not Y is None:
 		loss = tf.reduce_mean(tf.square(Y - _Y))
 
-		optimizer = tf.train.AdamOptimizer(0.005)
+		# optimizer = tf.train.AdamOptimizer(params['lr'])
+		optimizer = tf.train.GradientDescentOptimizer(params['lr'])
 		train_op = optimizer.minimize(loss, global_step = tf.train.get_global_step())
 	else:
 		loss = None
@@ -30,32 +29,33 @@ def my_model_fn(features, labels, mode, params, config):
 		)
 
 def my_input_fn_ds():
-	count = 10
-	X1 = [[random.random() * 100] for i in range(count)]
-	X2 = [[random.random() * 100] for i in range(count)]
-	Y = [X1[i][0] * 2 + X2[i][0] * 3 + 4 for i in range(count)]
+	count = 1000
+	X = [[random.random(), random.random()] for i in range(count)]
+	Y = [X[i][0] * 2 + X[i][1] * 3 + 4 for i in range(count)]
 
 	print("make train inputs dataset")
-	print('X1: ', X1)
-	print('X2: ', X2)
-	print('Y: ', Y)
+	# print('X: ', X)
+	# print('Y: ', Y)
 	# 可以通过numpy的对象直接创建数据集，这应该是正确的打开方式了
 	dataset = tf.data.Dataset.from_tensor_slices((
-		{'X1':X1, 'X2':X2},
+		{'X':X},
 		Y
 		))
 	dataset = dataset.repeat().batch(50)
 	return dataset
 
-def my_pred_input_fn():
-	features = [[float(i)] for i in range(20)]
-	print("make pred inputs")
-	print(features)
-	return (features, )
+def my_pred_input_fn(X):
+	def fn():
+		dataset = tf.data.Dataset.from_tensor_slices((
+			{'X':X},
+			))
+		dataset = dataset.batch(1)
+		return dataset
+	return fn
 
 def main():
 	tf.logging.set_verbosity(tf.logging.INFO) # 打开这句话，可以让训练过程中输出loss和步骤等信息
-	est = tf.estimator.Estimator(my_model_fn, model_dir='save_est/')
+	est = tf.estimator.Estimator(my_model_fn, model_dir='save_est/', params={'lr':0.01})
 	# train
 	est.train(my_input_fn_ds, steps=1000)
 
@@ -66,14 +66,16 @@ def main():
 	#print(est.evaluate(my_input_fn, steps=1))
 
 	# pred
-	# preds = est.predict(my_pred_input_fn)
-	# for i in range(20):
-	# 	print(next(preds))
+	X = [[float(i / 10.), float(i / 10.)] for i in range(5)]
+	print(X)
+	preds = est.predict(my_pred_input_fn(X))
+	print(list(preds))
 
 	# dump
 	names = est.get_variable_names()
 	for name in names:
-		print("\t%s: %f" % (name, est.get_variable_value(name)))
+		if not 'Adam' in name:
+			print("\t%s: %s" % (name, str(est.get_variable_value(name))))
 
 if __name__ == '__main__':
 	main()
